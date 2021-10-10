12968981c864c99883f0078ed06e05b93211902c
truyenho95 <truyenho.webdev@gmail.com>
2021-10-10 10:18:02 +0700

    - modify build.gradle - change db name

--- src/main/java/com/truyenhln/SavePersonHandler.java
+++ src/main/java/com/truyenhln/SavePersonHandler.java
@@ -2,6 +2,7 @@ package com.truyenhln;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
@@ -12,38 +13,37 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.truyenhln.bean.PersonRequest;
 import com.truyenhln.bean.PersonResponse;
+import java.util.Date;
 
 public class SavePersonHandler implements RequestHandler<PersonRequest, PersonResponse> {
-
     private DynamoDB dynamoDb;
 
-    private String DYNAMODB_TABLE_NAME = "Person";
+    private String DYNAMODB_TABLE_NAME = "HelloWorldDatabase";
+
     private Regions REGION = Regions.AP_SOUTHEAST_1;
 
     public PersonResponse handleRequest(PersonRequest personRequest, Context context) {
-        this.initDynamoDbClient();
-
+        String fullName = "Welcome " + personRequest.getFirstName() + " " + personRequest.getLastName();
+        initDynamoDbClient();
         persistData(personRequest);
-
         PersonResponse personResponse = new PersonResponse();
-        personResponse.setMessage("Saved Successfully!!!");
+        personResponse.setMessage(fullName);
         return personResponse;
     }
 
     private PutItemOutcome persistData(PersonRequest personRequest) throws ConditionalCheckFailedException {
-        return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
-          .putItem(
-            new PutItemSpec().withItem(new Item()
-              .withNumber("id", personRequest.getId())
-              .withString("firstName", personRequest.getFirstName())
-              .withString("lastName", personRequest.getLastName())
-              .withNumber("age", personRequest.getAge())
-              .withString("address", personRequest.getAddress())));
+        String name = personRequest.getFirstName() + " " + personRequest.getLastName();
+        Date date = new Date();
+        return this.dynamoDb.getTable(this.DYNAMODB_TABLE_NAME)
+                            .putItem((new PutItemSpec())
+                                             .withItem((new Item())
+                                                               .withString("ID", name)
+                                                               .withString("LatestGreetingTime", date.toString())));
     }
 
     private void initDynamoDbClient() {
         AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-        client.setRegion(Region.getRegion(REGION));
-        this.dynamoDb = new DynamoDB(client);
+        client.setRegion(Region.getRegion(this.REGION));
+        this.dynamoDb = new DynamoDB((AmazonDynamoDB) client);
     }
 }
